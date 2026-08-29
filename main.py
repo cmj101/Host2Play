c740c20d74f1b5b1f02316047d099a9c8be1696e
cmj101 <115442408+cmj101@users.noreply.github.com>
2026-08-29 11:23:32 +0800

    Update renewal URL in main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=f724dc02-b9f7-454b-9541-ee4b5d592a8f",
     # 添加更多链接
 ]
 
